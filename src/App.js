bd455eb010ff0d3661f64913ee7284220ce1339c
Nouman <noumanamir453@gmail.com>
2022-03-22 21:11:02 +0500

    Added api calls for file explorer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Homepage from "./components/Homepage/Homepage";
+import Test from "./Test";
 
 const App = () => {
   return(
-    <Homepage/>
+    <Test/>
   )
 }
 
---
+++ src/services/fileExplorer/apiCalls.js
@@ -0,0 +1,43 @@
+import axios from "../../axios";
+
+const baseURL = '/fileExplorer/'
+
+const createUserFolder = async (userID) => {
+    const url = baseURL + 'createUserFolder';
+    const res = await axios.post(url, {userID});
+}
+
+const createNewProject = async (userID, dirName) => {
+    const url = baseURL + 'createProjectDir';
+    const res = await axios.post(url, {userID, dirName})
+}
+
+const createNewFile = async (fileName, userID, currProjectName) => {
+    const url = baseURL + 'createFile';
+    const res = await axios.post(url, {fileName, userID, currProjectName})
+}
+
+const updateFile = async (code, userID, currProjectName, currFileName) => {
+    const url = baseURL + 'updateCode';
+    const res = await axios.post(url, {code, userID, currProjectName, currFileName})
+}
+
+const renameFile = async (userID, currFileName, newFileName, currProjectName) => {
+    const url = baseURL + 'renameFile';
+    const res = await axios.post(url, {userID, currFileName, currProjectName, newFileName})
+}
+
+const deleteFile = async (userID, fileName, currProjectName) => {
+    const url = baseURL + 'deleteFile';
+    const res = await axios.post(url, {userID, fileName, currProjectName});
+    console.log(res);
+}
+
+export {
+    createUserFolder,
+    createNewProject,
+    createNewFile,
+    updateFile,
+    renameFile,
+    deleteFile,
+}
