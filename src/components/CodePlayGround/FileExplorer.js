dc2e2d2a71706620d64fc0b06adcff001f6e9e50
Nouman <noumanamir453@gmail.com>
2022-03-28 17:04:50 +0500

    Added tooltip

--- src/components/CodePlayGround/FileExplorer.js
+++ src/components/CodePlayGround/FileExplorer.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from 'react';
+import ReactTooltip from 'react-tooltip';
 import {AiFillFolderAdd, AiFillFileAdd} from 'react-icons/ai'
 
 const FileExplorer = () => {
   return (
     <>
-        <div className='p-2' style={{width: '20%', height: '92%', backgroundColor: '#1c2333'}}>
+        <div className='p-2 overflow-auto' style={{width: '20%', height: '92%', backgroundColor: '#1c2333'}}>
             
             <div className='flex justify-between'>
                 <div className='ml-2 font-semibold text-2xl'>Files</div>
                 <div className='flex flex-row-reverse'>
-                    <div className='ml-2 py-2'><AiFillFileAdd size='20px'/></div>
-                    <div className='ml-2 py-2.5'><AiFillFolderAdd size='20px'/></div>
+                    <div className='ml-2 py-2'>
+                        <AiFillFileAdd size='20px' data-tip='New File'/>
+                        <ReactTooltip/>
+                    </div>
+                    <div className='ml-2 py-2.5'>
+                        <AiFillFolderAdd size='20px' data-tip='New Folder'/>
+                        <ReactTooltip/>
+                    </div>
                 </div>
             </div>
 
