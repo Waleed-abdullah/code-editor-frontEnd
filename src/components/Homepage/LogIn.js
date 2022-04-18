6f529349f7a77c3b8f825fe2833b7e0d00f5f29c
Nouman <noumanamir453@gmail.com>
2022-04-18 23:57:17 +0500

    div to btn

--- src/components/Homepage/LogIn.js
+++ src/components/Homepage/LogIn.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 const LogIn = () => {
   return (
-    <div style={{backgroundColor: "#0079F2"}}
+    <button style={{backgroundColor: "#0079F2"}}
     className='text-center w-28 rounded-xl 
     font-extrabold text-2xl font-mono mr-5
-    border-2 border-transparent hover:border-blue-400 hover:animate-pulse text-white'>
+    border-2 border-transparent hover:border-blue-400 text-white'>
+
         Log In
-    </div>
+    </button>
   )
 }
 
